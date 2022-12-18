e89f713a84f103dd3e3f9a049ef026ec068e5b8a
Đặng Trọng Đông <120403737+DangTrongDong@users.noreply.github.com>
2022-12-18 20:41:11 +0700

    Update script.js

--- matrix/script.js
+++ matrix/script.js
@@ -1,100 +1,43 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const canvas2 = document.getElementById('canvas2');
-const ctx2 = canvas2.getContext('2d');
-canvas2.width = window.innerWidth;
-canvas2.height = window.innerHeight;
-/*
-ctx2.shadowOffsetX = 1;
-ctx2.shadowOffsetY = 1;
-ctx2.shadowBlur = 0;
-ctx2.shadowColor = 'white';
-*/
-class Symbol {
-    constructor(x, y, fontSize, canvasHeight){
-        this.characters = '01010101000111110001010101110001110000011000100001ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678900101010001110001000100111111000100001001abcdefghijklmnopqrstuvwxyz0123456789';
-        this.x = x;
-        this.y = y;
-        this.fontSize = fontSize;
-        this.text = 'A';
-        this.canvasHeight = canvasHeight;
-        //this.color = 'hsl(' + this.x * 3+ ', 100%, 50%)';
-    }
-    draw(context, context2){
-        //context.font = this.fontSize + 'px monospace';
-        this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
-        //context.fillStyle = this.color;
-        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        context2.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.97){
-            this.y = 0;
-        }
-        else {
-            this.y += 0.9;
-        }
-    }
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const LATIN = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+const binary = '0101010101010101010101010101010101';
+const latin = "abcdefghijklmnopqrstuvwxyz"
+
+const alphabet = katakana + latin + nums + LATIN + binary;
+
+const fontSize = 16;
+const columns = canvas.width/fontSize;
+
+const rainDrops = [];
+
+for( let x = 0; x < columns; x++ ) {
+    rainDrops[x] = 1;
 }
 
-class Effect {
-    constructor(canvasWidth, canvasHeight){
-        this.fontSize = 16;
-        this.canvasWidth = canvasWidth;
-        this.canvasHeight = canvasHeight;
-        this.columns = this.canvasWidth/this.fontSize;
-        this.symbols = [];
-        this.#initialize();
-    }
-    #initialize(){
-        for (let i = 0; i < this.columns; i++) {
-            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+const draw = () => {
+    context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    
+    context.fillStyle = '#0F0';
+    context.font = fontSize + 'px monospace';
+
+    for(let i = 0; i < rainDrops.length; i++)
+    {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+        
+        if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+            rainDrops[i] = 0;
         }
+        rainDrops[i]++;
     }
-    resize(width, height){
-        this.canvasWidth = width;
-        this.canvasHeight = height;
-        this.columns = this.canvasWidth/this.fontSize;
-        this.symbols = [];
-        this.#initialize();
-    }
-}
-const effect = new Effect(canvas.width, canvas.height);
-let lastTime = 0;
-const fps = 26;
-const nextFrame = 1000/fps;
-let timer = 0;
+};
 
-function animate(timeStamp){
-    const deltaTime = timeStamp - lastTime;
-    lastTime = timeStamp;
-    if (timer > nextFrame){
-        ctx.textAlign = "center";
-        ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.font = effect.fontSize + 'px monospace';
-        ctx.fillStyle = '#03A062';
-        ctx.fillStyle = '#0aff0a';
-
-        ctx2.textAlign = "center";
-        ctx2.clearRect(0, 0, canvas.width, canvas.height);
-        ctx2.font = effect.fontSize + 'px monospace';
-        ctx2.fillStyle = 'white';
-
-        effect.symbols.forEach(symbol => symbol.draw(ctx, ctx2));
-        timer = 0;
-    } else {
-        timer += deltaTime;
-    }
-    requestAnimationFrame(animate);
-}
-animate(0);
-
-window.addEventListener('resize', function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvas2.width = window.innerWidth;
-    canvas2.height = window.innerHeight;
-    effect.resize(canvas.width, canvas.height);
-})
+setInterval(draw, 30);
